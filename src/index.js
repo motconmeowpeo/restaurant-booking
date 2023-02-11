98c89133065ccdd4417550383e78fe6a67497299
Trieu <trieut046@gmail.com>
2023-02-11 10:36:37 +0700

    nav footer

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js";
+import "popper.js/dist/umd/popper.min.js";
+import 'bootstrap/dist/js/bootstrap';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import HomeTemplate from "../client/pages";
+import Home from "../client/pages/Home";
+import Menu from "../client/pages/Menu";
+const HomeRoute = [
+    //Home
+    {
+        exact:true,
+        path:"/",
+        component:Home
+    },
+    {
+        exact:true,
+        path:"/menu",
+        component:Menu
+    }
+]
+const renderRouterHome = () =>{
+    return HomeRoute.map((root,index)=>{
+        return <HomeTemplate
+        key={index}
+        exact={root.exact}
+        path={root.path}
+        component={root.component}
+        />
+    })
+}
+export {renderRouterHome};
