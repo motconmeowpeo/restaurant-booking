77c13f885a52dc10e8c6333e409cdaf237e40d5e
Trieu <trieut046@gmail.com>
2023-02-22 19:19:09 +0700

    adjust menu

--- src/client/pages/Menu/index.js
+++ src/client/pages/Menu/index.js
@@ -259,9 +259,9 @@ export default function Menu() {
           </div>
         </div>
 
-        <div className="launch-content py-5">
-          <div className="launch-bg">
-                    LAUNCH
+        <div className="lunch-content py-5">
+          <div className="lunch-bg">
+                    LUNCH
           </div>
           <div className="row container">
             <div className="col-6">
